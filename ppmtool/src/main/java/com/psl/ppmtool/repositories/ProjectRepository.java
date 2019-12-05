42d09794c9aeeeb261694c765714ff323c1cb21e
ankit_yadav1 <ankit_yadav1@persistent.com>
2019-12-05 17:03:15 +0530

    added h2 database and interface

---
+++ ppmtool/src/main/java/com/psl/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.psl.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.psl.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+}
