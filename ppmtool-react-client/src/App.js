7721b2d41faa64ad4dd09437391bd093c348165a
ankit_yadav1 <ankit_yadav1@persistent.com>
2019-12-18 17:53:50 +0530

    Added project form

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/layout/Header';
-
+import {BrowserRouter as Router,Route} from 'react-router-dom'
+import AddProject from './components/Project/AddProject';
 function App() {
   return (
+    <Router>
     <div className="App">
       <Header/>
-      <Dashboard/>
+      <Route exact path="/Dashboard" component={Dashboard}/>
+      <Route exact path="/AddProject" component ={AddProject} />
     </div>
+    </Router>
   );
 }
 
